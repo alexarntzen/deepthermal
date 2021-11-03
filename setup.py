865b702b5e5117186fb34752a53f74895ceae860
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-11-03 13:30:30 +0100

    now possible without folds

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='deepthermal',
+    version='1.0',
+    packages=['test', 'deepthermal', 'task_solutions'],
+    url='',
+    license='MIT',
+    author='Alexander Johan Arntzen',
+    author_email='alexander.arntzen@gmail.com',
+    description='Project using deep learning to model thermal storage for solar panels '
+)
